6035337565f26c3480b437d7fe4b4fcd9216f64b
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-19 16:20:58 +0700

    init

--- app/src/main/java/com/aditya/watchit/data/FilmModel.kt
+++ app/src/main/java/com/aditya/watchit/data/FilmModel.kt
@@ -3,5 +3,6 @@ package com.aditya.watchit.data
 class FilmModel (
     var title: String,
     var description: String,
-    var banner: Int
+    var banner: Int,
+    var watchCount: Int
         )
