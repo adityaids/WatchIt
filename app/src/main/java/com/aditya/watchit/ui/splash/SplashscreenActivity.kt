59fb80ac33b97dd41aa7ce6e468c643591577465
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-23 23:57:16 +0700

    fix all

--- app/src/main/java/com/aditya/watchit/ui/splash/SplashscreenActivity.kt
+++ app/src/main/java/com/aditya/watchit/ui/splash/SplashscreenActivity.kt
@@ -1,12 +1,20 @@
 package com.aditya.watchit.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.aditya.watchit.R
+import com.aditya.watchit.ui.main.MainActivity
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashscreenActivity, MainActivity::class.java))
+            finish()
+        }, 10000L)
     }
 }
