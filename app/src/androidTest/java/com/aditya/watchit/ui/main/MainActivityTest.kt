f34f1e71508c279acbe1d1f4e962c4003122749c
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-24 00:13:02 +0700

    fix all

--- app/src/androidTest/java/com/aditya/watchit/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/aditya/watchit/ui/main/MainActivityTest.kt
@@ -68,6 +68,7 @@ class MainActivityTest{
         onView(withId(R.id.rv_tv_series)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
         onView(withId(R.id.tv_detail_title)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.tv_detail_title)).check(ViewAssertions.matches(withText(dummyTv[0].title)))
+        onView(withId(R.id.tv_type)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.tv_type)).check(ViewAssertions.matches(withText(dummyTv[0].type)))
         onView(withId(R.id.tv_detail_description)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.tv_detail_description)).check(ViewAssertions.matches(withText(dummyTv[0].description)))
